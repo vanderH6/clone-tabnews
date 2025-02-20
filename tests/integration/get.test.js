6d7a6db1dceb58bee8a9bd2c1a5209ed063fe5f6
vanderH6 <meneargo.tecnologias@gmail.com>
2025-02-20 23:19:19 +0000

    Adicionando `database.js`, `compose.yml` e `env`

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,6 +1,6 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch(
-    "https://super-duper-eureka-pjw94xp47qrv3rq64-3000.app.github.dev/api/status",
+    "https://super-duper-eureka-pjw94xp47qrv3rq64-3000.app.github.dev/api/v1/status",
   );
   expect(response.status).toBe(200);
 });
